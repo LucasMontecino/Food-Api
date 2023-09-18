752174669a9d8e6f7d90ba12dd17aadafc1e1329
Lucas Montecino <lucasmontecino95@gmail.com>
2023-09-18 11:10:33 -0300

    fix6

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -4,13 +4,8 @@ import style from "./Loading.module.css";
 const Loading = () => {
   return (
     <div className={style.loading_container}>
-      <p>Loading...</p>
       <div className={style.loading_image}></div>
-      {/* <img
-        src="https://media.giphy.com/media/83xGKHnlQa5yw/giphy.gif"
-        alt="Loading..."
-        className={style.loading_image}
-      /> */}
+      <p>Loading...</p>
     </div>
   );
 };
