6e08c0186de8c59d26eaf1a0cf4b5ef4a03d5566
Lucas Montecino <lucasmontecino95@gmail.com>
2024-12-08 20:57:43 -0300

    update - indexjs api

--- api/index.js
+++ api/index.js
@@ -17,14 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const routes = require("./src/routes/index.js");
-const { conn } = require("./src/db.js");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./src/routes/index.js');
+const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 const port = PORT || 3001;
 
@@ -35,29 +35,38 @@ conn.sync({ force: false }).then(() => {
   });
 });
 
-require("./src/db.js");
+require('./src/db.js');
 
 const server = express();
 
-server.name = "API";
+server.name = 'API';
 
 server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(
+  bodyParser.urlencoded({ extended: true, limit: '50mb' })
+);
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://food-api-iota.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Origin',
+    'https://food-api-iota.vercel.app'
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  );
   next();
 });
 
-server.use("/", routes);
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
