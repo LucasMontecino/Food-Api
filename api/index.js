dee3132fca7ac34cff68b3823c2ee38251da9fe7
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-22 07:57:34 -0300

    refactor - remove express server setup and replace console logging with logger

--- api/index.js
+++ api/index.js
@@ -17,64 +17,18 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./src/routes/index.js');
+require('./src/db.js');
+const { PORT } = require('./src/utils/config.js');
+const server = require('./app.js');
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
+const logger = require('./src/utils/logger.js');
+
 const port = PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+    logger.info(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
-
-require('./src/db.js');
-
-const server = express();
-
-server.name = 'API';
-
-server.use(cors());
-server.use(
-  bodyParser.urlencoded({ extended: true, limit: '50mb' })
-);
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://food-api-iota.vercel.app'
-  );
-  // res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
-
-server.use('/', routes);
-
-// Error catching endware.
-server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-module.exports = server;
